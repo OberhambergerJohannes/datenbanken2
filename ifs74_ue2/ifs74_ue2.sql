6950a7ab2d1cc489235d702706ec2c64ce6c3053
job <oberbergerhannes@gmail.com>
2025-10-12 21:12:39 +0200

    JOB | edited UE2

--- ifs74_ue2/ifs74_ue2.sql
+++ ifs74_ue2/ifs74_ue2.sql
@@ -26,3 +26,18 @@ INSERT INTO jobs (job_id, job_title, min_salary, max_salary) VALUES ('EN_COOLBRO
 INSERT INTO jobs (job_id, job_title, min_salary, max_salary) VALUES ('FR_COOLBRO', 'Megacoolbro', 10000, 1000);
 
 --1b
+CREATE OR REPLACE TRIGGER createJobHistoryTrigger
+AFTER UPDATE OF job_id, department_id on employees
+FOR EACH ROW
+WHEN (NEW.job_id != OLD.job_id OR NEW.department_id != OLD.department_id)
+DECLARE
+    employee_date DATE;
+BEGIN
+    SELECT MAX(end_date) INTO employee_date FROM job_history WHERE employee_id = OLD.employee_id;
+IF
+    employee_date IS NULL THEN
+    SELECT hire_date INTO employee_date FROM employees WHERE employee_id = OLD.employee_id;
+END IF;
+INSERT INTO job_history (employee_id, start_date, end_date, job_id, department_id)
+VALUES (OLD.employee_id, employee_date, SYSDATE, OLD.job_id, OLD.department_id);
+END createJobHistoryTrigger;
